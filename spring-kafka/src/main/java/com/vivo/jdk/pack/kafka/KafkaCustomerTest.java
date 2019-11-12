270467cbafa871556062d4c0a34a4e6894548cb3
汤旗 <tangqihlw@bbktel.com>
2019-11-12 20:51:54 +0800

    kafka学习测试

--- spring-kafka/src/main/java/com/vivo/jdk/pack/kafka/KafkaCustomerTest.java
+++ spring-kafka/src/main/java/com/vivo/jdk/pack/kafka/KafkaCustomerTest.java
@@ -3,13 +3,11 @@ package com.vivo.jdk.pack.kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
-import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Properties;
 
 /**
  * 类描述：kafka消费者测试类
@@ -35,9 +33,6 @@ public class KafkaCustomerTest {
         consumer.assign(Arrays.asList(partition0));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            Map<MetricName, ? extends Metric> metricData = consumer.metrics();
-            Map<String, List<PartitionInfo>> topicData = consumer.listTopics();
-            Set<TopicPartition> dataSet = consumer.assignment();
             for (ConsumerRecord<String, String> record : records) {
                 System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value() + "\n");
             }

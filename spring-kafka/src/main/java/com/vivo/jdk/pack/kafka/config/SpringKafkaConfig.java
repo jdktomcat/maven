d15706ba802dfe42f4aa9ac083544a201456d17c
汤旗 <tangqihlw@bbktel.com>
2019-11-26 10:05:54 +0800

    kafka学习测试

--- spring-kafka/src/main/java/com/vivo/jdk/pack/kafka/config/SpringKafkaConfig.java
+++ spring-kafka/src/main/java/com/vivo/jdk/pack/kafka/config/SpringKafkaConfig.java
@@ -1,19 +1,8 @@
 package com.vivo.jdk.pack.kafka.config;
 
-import lombok.extern.log4j.Log4j;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.messaging.Message;
 
 /**
  * 类描述：
@@ -21,25 +10,8 @@ import org.springframework.util.backoff.FixedBackOff;
  * @author 汤旗
  * @date 2019-11-23 17:42
  */
-@Log4j
 @Configuration
 public class SpringKafkaConfig {
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
-            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-            ConsumerFactory<Object, Object> kafkaConsumerFactory,
-            KafkaTemplate<Object, Object> template) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        configurer.configure(factory, kafkaConsumerFactory);
-        factory.setErrorHandler(new SeekToCurrentErrorHandler(new DeadLetterPublishingRecoverer(template), new FixedBackOff(0L, 2)));
-        return factory;
-    }
-
-    @Bean
-    public RecordMessageConverter converter() {
-        return new StringJsonMessageConverter();
-    }
 
     /**
      * kafka主题监控监听器回调函数
@@ -47,13 +19,7 @@ public class SpringKafkaConfig {
      * @param message 消息
      */
     @KafkaListener(id = "fooGroup", topics = "ads-marketing-operation-log-dev")
-    public void listen(String message) {
-        log.info("Received: " + message);
+    public void listen(Message<String> message) {
+        System.out.println("Received: " + message);
     }
-
-    @Bean
-    public NewTopic topic() {
-        return new NewTopic("ads-marketing-operation-log-dev", 1, (short) 1);
-    }
-
 }
---
+++ spring-kafka/src/main/java/com/vivo/jdk/pack/kafka/config/SpringKafkaCustomerConfig.java
@@ -0,0 +1,57 @@
+package com.vivo.jdk.pack.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+/**
+ * 类描述：kafka消费者配置
+ *
+ * @author 汤旗
+ * @date 2019-11-26 09:56
+ */
+@Configuration
+@EnableKafka
+public class SpringKafkaCustomerConfig {
+
+    private static final String GROUP_ID_CONFIG = "fooGroup";
+
+    private static final String AUTO_OFFSET_RESET_CONFIG = "earliest";
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CONFIG);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET_CONFIG);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}

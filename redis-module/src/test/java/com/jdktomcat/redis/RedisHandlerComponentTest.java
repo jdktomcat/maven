86484494524325cc37aa5a07d0e970a84e82235c
汤旗 <tangqihlw@bbktel.com>
2020-03-13 21:00:04 +0800

    redis测试学习

--- redis-module/src/test/java/com/jdktomcat/redis/RedisHandlerComponentTest.java
+++ redis-module/src/test/java/com/jdktomcat/redis/RedisHandlerComponentTest.java
@@ -1,11 +1,15 @@
 package com.jdktomcat.redis;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.JedisCluster;
+
+import java.util.*;
 
 /**
  * 类描述：redis测试类
@@ -27,9 +31,20 @@ public class RedisHandlerComponentTest {
     public void test() {
         String sourceList = "source-set-1";
         String disList = "{" + sourceList + "}:BAK";
+
+        int pageSize = 100;
+        List<String> dataList = new ArrayList<>(pageSize);
+        long startTime = System.currentTimeMillis();
         while (true) {
             String message = jedisCluster.brpoplpush(sourceList, disList, 0);
-            System.out.println(message);
+            if (StringUtils.isNotBlank(message)) {
+                dataList.add(message);
+            }
+            if (dataList.size() >= 100 || System.currentTimeMillis() - startTime > 2000) {
+                System.out.println(Arrays.toString(dataList.toArray()));
+                dataList.clear();
+                startTime = System.currentTimeMillis();
+            }
             jedisCluster.lrem(disList, 1, message);
         }
     }

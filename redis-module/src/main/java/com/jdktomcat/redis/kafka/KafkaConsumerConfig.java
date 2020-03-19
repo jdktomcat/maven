2b0d01f5d07caf6d300b7b1beb0602e2ae16d75e
汤旗 <tangqihlw@bbktel.com>
2020-03-19 11:39:06 +0800

    kafka测试学习

--- redis-module/src/main/java/com/jdktomcat/redis/kafka/KafkaConsumerConfig.java
+++ redis-module/src/main/java/com/jdktomcat/redis/kafka/KafkaConsumerConfig.java
@@ -1,21 +1,17 @@
 package com.jdktomcat.redis.kafka;
 
-import kafka.consumer.Consumer;
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
 import lombok.Data;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 /**
  * 类描述：kafka配置
@@ -26,12 +22,16 @@ import java.util.concurrent.Executors;
 @Data
 @Component
 public class KafkaConsumerConfig {
+    /**
+     * 日志
+     */
+    private static final Logger logger = Logger.getLogger(KafkaConsumerConfig.class);
 
     /**
      * zookeeper地址配置
      */
-    @Value("${kafka.zookeeper.address}")
-    private String zk;
+    @Value("${kafka.server.address}")
+    private String kafkaServer;
 
     /**
      * 主题
@@ -44,6 +44,12 @@ public class KafkaConsumerConfig {
      */
     @Value("${kafka.customer.group.id:ads_marketing}")
     private String groupId;
+
+    /**
+     * 客户端标识
+     */
+    @Value("${kafka.customer.client.id:ads_marketing}")
+    private String clientId;
 
     /**
      * zk会话超时时长
@@ -63,34 +69,47 @@ public class KafkaConsumerConfig {
     @Value("${kafka.auto.commit.interval:1000}")
     private Long autoCommitInterval;
 
+    /**
+     * 一次获取条数
+     */
+    @Value("${kafka.max.poll.records:1000}")
+    private Integer maxPollRecords;
+
+
+    /**
+     * 获取配置信息
+     *
+     * @return 配置
+     */
+    private Properties getConfigProperties() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", kafkaServer);
+        props.put("group.id", groupId);
+        props.put("client.id", clientId);
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.commit.interval.ms", autoCommitInterval);
+        props.put("max.poll.records", maxPollRecords);
+        logger.info(String.format("kafka消费者配置信息：%s", props.toString()));
+        return props;
+    }
 
     /**
      * 初始化
      */
     @PostConstruct
     public void init() {
-        Properties props = new Properties();
-        props.put("zookeeper.connect", zk);
-        props.put("group.id", groupId);
-        props.put("zookeeper.session.timeout.ms", zkSessionTimeout);
-        props.put("zookeeper.connection.timeout.ms", zkSessionTimeout);
-        props.put("zookeeper.sync.time.ms", zkSyncTime);
-        props.put("auto.commit.interval.ms", autoCommitInterval);
-        ConsumerConnector consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
-        Map<String, Integer> topicCountMap = new HashMap<>();
-        topicCountMap.put(topic, new Integer(1));
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
-        final ConsumerIterator<byte[], byte[]> it = stream.iterator();
-        Executor executor = Executors.newSingleThreadExecutor();
-        executor.execute(() -> {
-            while (it.hasNext()) {
-                System.out.println("************** receive：" + new String(it.next().message()));
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Properties props = this.getConfigProperties();
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(Arrays.asList(topic), new ConsumerRebalanceListener() {
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                logger.info("revoked:" + partitions.size());
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                logger.info("assigned:" + partitions.size());
             }
         });
     }

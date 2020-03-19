f87f464fe6a2dde9691f69e1ab68b999c203bfd6
汤旗 <tangqihlw@bbktel.com>
2020-03-19 17:50:55 +0800

    kafka测试学习

---
+++ kafka-demo/src/main/java/com/jdktomcat/redis/SpringKafkaContextRunner.java
@@ -0,0 +1,20 @@
+package com.jdktomcat.redis;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * 类描述：SpringContext执行器
+ *
+ * @author 11072131
+ * @date 2020-03-2020/3/17 11:10
+ */
+public class SpringKafkaContextRunner {
+
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-application.xml");
+        System.in.read();
+        applicationContext.close();
+    }
+}
---
+++ kafka-demo/src/main/java/com/jdktomcat/redis/kafka/SpringKafkaCustomerConfig.java
@@ -0,0 +1,69 @@
+package com.jdktomcat.redis.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类描述：kafka消费者配置
+ *
+ * @author 汤旗
+ * @date 2019-11-26 09:56
+ */
+@Configuration
+@EnableKafka
+public class SpringKafkaCustomerConfig {
+    /**
+     * 消费组
+     */
+    public static final String GROUP_ID_CONFIG = "ads-marketing";
+
+    private static final String AUTO_OFFSET_RESET_CONFIG = "earliest";
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CONFIG);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET_CONFIG);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 5000);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public KafkaStreamsConfiguration defaultKafkaStreamsConfig() {
+        Map<String, Object> dataMap = consumerConfigs();
+        dataMap.put("application.id","ads");
+        return new KafkaStreamsConfiguration(dataMap);
+    }
+}

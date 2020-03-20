c0476ae46afea0ff0efbd549285abdd8aad5896f
汤旗 <tangqihlw@bbktel.com>
2020-03-20 11:41:25 +0800

    kafka测试学习

--- kafka-demo/kafka-consumer-demo/src/main/java/com/jdktomcat/pack/kafka/consumer/listener/SpringKafkaConsumerListener.java
+++ kafka-demo/kafka-consumer-demo/src/main/java/com/jdktomcat/pack/kafka/consumer/listener/SpringKafkaConsumerListener.java
@@ -1,8 +1,8 @@
 package com.jdktomcat.pack.kafka.consumer.listener;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.protocol.Message;
 import org.apache.log4j.Logger;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -16,22 +16,12 @@ import java.util.List;
  * @date 2020-03-2020/3/20 11:07
  */
 @Component
+@EnableKafka
 public class SpringKafkaConsumerListener {
     /**
      * 日志
      */
     private static final Logger logger = Logger.getLogger(SpringKafkaConsumerListener.class);
-
-
-    /**
-     * kafka主题监控监听器回调函数
-     *
-     * @param message 消息
-     */
-    @KafkaListener(topics = "send_click_topic", containerFactory = "consumerFactory")
-    public void listen(String message) {
-        logger.info("接收到消息：" + message);
-    }
 
     /**
      * kafka主题监控监听器回调函数
@@ -42,7 +32,7 @@ public class SpringKafkaConsumerListener {
     @KafkaListener(topics = "send_click_topic", containerFactory = "batchFactory")
     public void listenBatch(List<ConsumerRecord<String, String>> records, Acknowledgment ack) {
         for (ConsumerRecord<String, String> record : records) {
-            logger.info("Received: " + record.key() + "====" + record.value());
+            logger.info(String.format("消费者获取，消息offset:%s,消息键:%s,消息体：%s", record.offset(), record.key(), record.value()));
         }
         ack.acknowledge();
     }

ed03b8fba819a006af76268aec67471d925af784
汤旗 <tangqihlw@bbktel.com>
2020-04-09 14:40:44 +0800

    接收数据测试

--- receive-data-module/src/main/java/com/jdktomcat/receive/data/module/controller/ReceiveDataController.java
+++ receive-data-module/src/main/java/com/jdktomcat/receive/data/module/controller/ReceiveDataController.java
@@ -2,11 +2,12 @@ package com.jdktomcat.receive.data.module.controller;
 
 import com.jdktomcat.receive.data.module.entry.ClickDataInfo;
 import com.jdktomcat.receive.data.module.response.ResponseEntry;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * 类描述：接收数据测试接口
@@ -20,13 +21,15 @@ public class ReceiveDataController {
     /**
      * 数据接收接口
      *
+     * @param receiveData 接收数据
      * @return 响应体
      */
     @RequestMapping(method = RequestMethod.POST, value = "/receive/data")
-    public ResponseEntry receiveData(List<ClickDataInfo> receiveDataList) {
+    public ResponseEntry receiveData(@RequestBody ClickDataInfo[] receiveData) {
         ResponseEntry responseEntry = new ResponseEntry();
         responseEntry.setCode("0");
         responseEntry.setMsg("数据接收成功！");
+        System.out.println(String.format("接收数据：%s", Arrays.toString(receiveData)));
         return responseEntry;
     }
 }

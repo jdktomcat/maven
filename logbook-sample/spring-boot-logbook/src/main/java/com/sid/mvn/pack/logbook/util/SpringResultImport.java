9e7bd40fbec9e623091d6eafa0999396c0893ae8
tangqi <tangqi.tang@alibaba-inc.com>
2021-11-02 20:51:33 +0800

    代码优化

---
+++ logbook-sample/spring-boot-logbook/src/main/java/com/sid/mvn/pack/logbook/util/SpringResultImport.java
@@ -0,0 +1,18 @@
+package com.sid.mvn.pack.logbook.util;
+
+import com.amap.dataframework.data.support.Result;
+
+/**
+ * 类概述：
+ *
+ * @author tangqi
+ * @date 2021-10-12
+ */
+public class SpringResultImport {
+
+
+    public void  test(){
+        Result  result = new Result();
+    }
+
+}

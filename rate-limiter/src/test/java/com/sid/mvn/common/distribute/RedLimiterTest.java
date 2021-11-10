40d49f4b229a6c8ddb8117fc8fbaa17d225c8efc
tangqi <tangqi.tang@alibaba-inc.com>
2021-11-10 19:14:48 +0800

    代码优化

---
+++ rate-limiter/src/test/java/com/sid/mvn/common/distribute/RedLimiterTest.java
@@ -0,0 +1,93 @@
+package com.sid.mvn.common.distribute;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 类概述：
+ *
+ * @author tangqi
+ * @date 2021-11-10
+ */
+public class RedLimiterTest {
+
+    private static RedLimiter limiter;
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(500);
+
+    @BeforeClass
+    public static void init() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(200);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, "localhost");
+        limiter = RedLimiter.create("1000", 1, jedisPool, true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(2000L);
+    }
+
+
+
+    @org.junit.Test
+    public void acquire() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            pool.execute(() -> {
+                double acquire = limiter.acquire(1);
+                System.out.println(index + " \t" + acquire + " \t" + new Date());
+            });
+        }
+        Thread.sleep(12 * 1000L);
+    }
+
+    @org.junit.Test
+    public void tryAcquire() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            pool.execute(() -> {
+                boolean acquire = limiter.tryAcquire();
+                System.out.println(index + " \t" + acquire + " \t" + new Date());
+            });
+        }
+        Thread.sleep(5 * 1000L);
+    }
+
+    @org.junit.Test
+    public void tryAcquireTimeout() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            pool.execute(() -> {
+                boolean acquire = limiter.tryAcquire(1000L, TimeUnit.MILLISECONDS);
+                System.out.println(index + " \t" + acquire + " \t" + new Date());
+            });
+        }
+        Thread.sleep(10 * 1000L);
+    }
+
+    @Test
+    public void batchAcquireLazy() throws Exception {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(50);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, "localhost");
+        RedLimiter redLimiter = RedLimiter.create("500", 1000, jedisPool, true);
+        redLimiter.setBatchSize(50);
+        for (int i = 0; i < 1000; i++) {
+            final int index = i;
+            pool.execute(() -> {
+                double acquire = redLimiter.acquireLazy(10);
+                System.out.println(index + " \t" + acquire + " \t" + new Date());
+            });
+        }
+        Thread.sleep(10 * 1000L);
+    }
+}
